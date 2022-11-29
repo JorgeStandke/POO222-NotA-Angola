1c33f09fea2368cd9102de4919f5edcfb6003f9c
JorgeStandke <67119973+JorgeStandke@users.noreply.github.com>
2022-11-28 21:24:36 -0300

    arrumar
    Precisa achar um jeito de deixar a variável do metodo da classe team visivel para o foreach do MAP

--- Marco4/Models/Team.java
+++ Marco4/Models/Team.java
@@ -5,7 +5,14 @@ import java.util.HashMap;
 public class Team {
     HashMap<Integer, Player> players;
     HashMap<String, Technical> technicals;
+   
 
+    public int playersQuantity(){
+        return this.players.size();
+    }
+    public int technicalsquantity(){
+        return this.technicals.size();
+    }
     public Player getPlayers(int number) {
         return players.get(number);
     }
@@ -18,5 +25,22 @@ public class Team {
     public void setTechnicals(Technical technical) {
         this.technicals.put(technical.getName(), technical);
     }
-  
+    public int oldestPlayer(){
+        int age = Integer.MIN_VALUE;
+        players.forEach((key, value)  -> {
+            if(value.getAge() > age){
+                age = value.getAge();
+            }
+        } );
+        return age;
+    }
+    public int youngestPlayer(){
+        int age = Integer.MIN_VALUE;
+        players.forEach((key, value) -> {
+            if(value.getAge() > age){
+                age = value.getAge();
+            }
+        } );
+        return age;
+    }
 }
--- Marco4/Programa.java
+++ Marco4/Programa.java
@@ -1,8 +1,12 @@
 package Marco4;
+import java.nio.file.Path;
+
+import javax.imageio.ImageIO;
+
 import Marco4.Interfaces.*;
 import Marco4.Models.*;
 
-public class Programa implements NationalTeamStats {
+public class Programa implements NationalTeamStats, NationalTeamInfos {
 
 	private Country country = new Country("Angola");
 
@@ -27,5 +31,63 @@ public class Programa implements NationalTeamStats {
 
         questionsCount++;
 
+    }
+
+    @Override
+    public int getHowManyMembers() {
+        return country.getSoccerTeam().playersQuantity() + country.getSoccerTeam().technicalsquantity();      
+    }
+
+    @Override
+    public int getOldestPlayer() {
+        return country.getSoccerTeam().oldestPlayer();
+    }
+
+    @Override
+    public int getYoungestPlayer() {
+
+        return 0;
+    }
+
+    @Override
+    public double getAverageAge() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public String getPlayer(int number) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getPressOfficerContacts() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getCountryName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ImageIO getFlagImage() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Path getTechnicalCommittee() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public NationalTeamStats getStatsResponsible() {
+        // TODO Auto-generated method stub
+        return null;
     }
 }
